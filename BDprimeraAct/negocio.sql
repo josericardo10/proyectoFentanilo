104addb5b0ecf67475daaf35e6486ac925f767bc
josericardo10 <135680364+josericardo10@users.noreply.github.com>
2024-09-05 09:00:09 -0600

    Update negocio.sql

--- BDprimeraAct/negocio.sql
+++ BDprimeraAct/negocio.sql
@@ -1,27 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 05-09-2024 a las 08:13:36
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Base de datos: `negocio`
 --
-
--- --------------------------------------------------------
+CREATE DATABASE negocio;
+USE negocio;
 
 --
 -- Estructura de tabla para la tabla `cliente`
