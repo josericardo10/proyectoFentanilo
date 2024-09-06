f6dca0be7a8b538fa06033150280d80b26b21937
josericardo10 <135680364+josericardo10@users.noreply.github.com>
2024-09-05 23:45:07 -0600

    inicio de la pagina de laberinto y su estructura basica ;D

---
+++ laberinto/js/script.js
@@ -0,0 +1,7 @@
+function Perdedor(){
+    document.write('perdiste');
+    document.write('<img src="assets/img/perdedor.gif" class="per">');
+}
+function Ganador(){
+    document.write('ganaste');
+}
