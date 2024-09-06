367626ea6e0f42455577a5b7a676c3eb0005f334
josericardo10 <135680364+josericardo10@users.noreply.github.com>
2024-09-05 21:54:42 -0600

    base de datos consecionaria y registros de cliente

---
+++ BDConsecionaria/registrosCliente.sql
@@ -0,0 +1,13 @@
+--crear registros de cliente
+INSERT INTO cliente (codigo,nombre,direccion,ciudad, telefono) 
+VALUES
+(NULL,'ricardo','atizapan', 'mexico',5531910154),
+(NULL,'ares', 'nicolar romero', 'mexico', 5698874156),
+(NULL,'jaqueline', 'nicolas romero','mexico',5539875212),
+(NULL, 'carlos','nicolas romero','usa', 5692356587),
+(NULL,'marely','francia','paris',1025252536),
+(NULL,'luis','san predro','españa',3232659744),
+(NULL,'erick','san pedro','mexico',4550203620),
+(NULL,'katy','torres','mexico',5454841200),
+(NULL,'antonio','la merced','inglaterra',5878741054),
+(NULL,'ernesto','posos','texas',1547582013);
